27344e770225de86bf8de6cde61515da776d1cb4
SOBER <2018302180106@whu.edu.cn>
2019-09-11 00:48:47 +0800

    upload basic site files

---
+++ mysite/library/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+app_name = 'library'
+urlpatterns = [
+    path('', views.log_in),
+    path('library', views.library_form, name='library'),
+]
---
+++ mysite/library/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+def log_in(request):
+    if request.method == 'GET':
+        return render(request,'log_in.html')
+    elif request.method == 'POST':
+        return redirect(reverse('library:library'))
+    
+
+def library_form(request):
+    if request.method == 'GET':
+        return render(request,'text.html')
+    elif request.method == 'POST':
+       # print(request.POST.get('library'))
+        return HttpResponse(request.POST.get('library'))
+# Create your views here.
